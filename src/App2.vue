97bfdfa257c4cea09fd8be937c3760dd093ee61d
Manuel Palma Mora <palmamora001@gmail.com>
2024-07-30 18:33:53 -0400

    first commit

---
+++ src/App2.vue
@@ -0,0 +1,53 @@
+<script>
+export default {
+  data() {
+    return {
+      name: "john",
+      status: 'active',
+      tasks: [
+        'Task One',
+        'Task Two',
+        'Task Three'
+      ],
+      link: "http://google.com"
+    }
+  },
+  methods: {
+    toggleStatus() {
+      if (this.status === 'active') {
+        this.status = 'pending';
+      } else if (this.status === 'pending') {
+        this.status = 'inactive';
+      } else {
+        this.status = 'active'
+      }
+
+    }
+  }
+}
+</script>
+
+<template>
+  <h1>vue jobs, {{ name }}</h1>
+  <p v-if="status === 'active'">User is active</p>
+  <p v-else-if="status === 'pending'">User is pending</p>
+  <p v-else>User is inactive</p>
+
+
+  <h3>Tasks: </h3>
+  <ul>
+    <li v-for="task in tasks" :key="task">
+      {{ task }}
+    </li>
+  </ul>
+  <a :href="link">click</a>
+
+  <!-- <button v-on:click="toggleStatus" >change status</button> -->
+  <button @click="toggleStatus" >change status</button>
+</template>
+
+<style scoped>
+h1 {
+  color: red;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup>
+import Hero from '@/components/Hero.vue';
+import HomeCards from '@/components/HomeCards.vue';
+import JobListings from '@/components/JobListings.vue';
+</script>
+
+<template>
+    <Hero title="Hello world!" subtitle="Find the Vue job that fits your skills and needs" />
+    <HomeCards />
+    <JobListings :limit="3" :showButton="true" />
+</template>
