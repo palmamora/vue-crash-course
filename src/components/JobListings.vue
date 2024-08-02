dc4a5051e6c30bbf093cff1a9bcb402aefa9bc32
Manuel Palma Mora <palmamora001@gmail.com>
2024-08-02 01:22:23 -0400

    new changes

--- src/components/JobListings.vue
+++ src/components/JobListings.vue
@@ -1,14 +1,31 @@
 <script setup>
-import jobData from '@/jobs.json';
-import { ref, defineProps } from 'vue';
+import { reactive, defineProps, onMounted } from 'vue';
 import JobListing from '@/components/JobListing.vue';
+import { RouterLink } from 'vue-router';
+import axios from 'axios';
+import PulseLoader from 'vue-spinner/src/PulseLoader.vue';
 
-const jobs = ref(jobData);
 defineProps({
     limit: Number,
     showButton: {
         type: Boolean,
         default: false
+    }
+})
+const state = reactive({
+    jobs: [],
+    isLoading: true
+})
+
+onMounted(async () => {
+    try {
+        const res = await axios.get('/api/jobs');
+        //jobs.value = res.data;
+        state.jobs = res.data
+    } catch (error) {
+        console.error('error fetching jobs', error);
+    } finally {
+        state.isLoading = false;
     }
 })
 </script>
@@ -19,15 +36,21 @@ defineProps({
             <h2 class="text-3xl font-bold text-green-500 mb-6 text-center">
                 Browse Jobs
             </h2>
-            <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
-                <JobListing v-for="(job, index) in jobs.slice(0, limit || jobs.length)" :key="job.id" :job="job" />
+            <!--show loading spinner-->
+            <div v-if="state.isLoading" class="text-center text-gray-500">
+                <PulseLoader />
+            </div>
+            <!--show content-->
+            <div v-else class="grid grid-cols-1 md:grid-cols-3 gap-6">
+                <JobListing v-for="(job, index) in state.jobs.slice(0, limit || state.jobs.length)" :key="job.id"
+                    :job="job" />
             </div>
         </div>
     </section>
 
     <section v-if="showButton" class="m-auto max-w-lg my-10 px-6">
-        <a href="/jobs" class="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700">View
-            All Jobs</a>
+        <RouterLink to="/jobs" class="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700">View
+            All Jobs</RouterLink>
     </section>
 
 </template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,9 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "@/views/HomeView.vue";
+import JobsView from "@/views/JobsView.vue";
+import NotFoundView from "@/views/NotFoundView.vue";
+import JobView from "@/views/JobView.vue";
+import AddJobView from "@/views/AddJobView.vue";
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +12,26 @@ const router = createRouter({
             path: '/',
             name: 'home',
             component: HomeView
+        },
+        {
+            path: '/jobs',
+            name: 'jobs',
+            component: JobsView
+        },
+        {
+            path: '/jobs/:id',
+            name: 'job',
+            component: JobView
+        },
+        {
+            path: '/jobs/add',
+            name: 'job-add',
+            component: AddJobView
+        },
+        {
+            path: '/:catchAll(.*)',
+            name: 'not-found',
+            component: NotFoundView
         }
     ]
 });
